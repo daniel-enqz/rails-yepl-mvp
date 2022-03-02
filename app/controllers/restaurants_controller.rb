4a94d1aa68013976678ee9ecde76fe9d7818fe9f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-02 14:30:32 -0600

    finished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,14 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
-  # GET /restaurants
   def index
     @restaurants = Restaurant.all
   end
 
   # GET /restaurants/1
-  def show; end
+  def show
+    @review = Review.new
+  end
 
   # GET /restaurants/new
   def new
