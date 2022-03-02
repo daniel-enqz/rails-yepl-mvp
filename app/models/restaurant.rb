88c726bb5889106d9666f1c9ba3f0a1c259d0237
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-02 12:30:18 -0600

    restaurant changed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
                                     message: `%{value} is not a valid size` }
