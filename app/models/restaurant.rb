fc7f171aa0e1784ed1c1261bf6f382cba6032d29
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-02 12:26:34 -0600

    phone number type changed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,5 @@ class Restaurant < ApplicationRecord
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
                                     message: `%{value} is not a valid size` }
+  validates :phone_number, format: { with: /\d[0-9]\)*\z/, message: "Only positive number without spaces are allowed"}
 end
