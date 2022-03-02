99a1d1425bc8f611159e45ca75273419cf90796f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-02 14:14:55 -0600

    reviews route fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,4 @@
 class ReviewsController < ApplicationController
-  def new
-    # we need @restaurant in our `simple_form_for`
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     # we need `restaurant_id` to associate review with corresponding restaurant
