36ff127082195b59a921d2e02dc17e4a613e3692
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-02 12:36:36 -0600

    restaurant changed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
-                                    message: `%{value} is not a valid size` }
-  validates :phone_number, format: { with: /\d[0-9]\)*\z/, message: "Only positive number without spaces are allowed"}
+                                    message: 'Restaurant category not valid!' }
+  validates :phone_number, format: { with: /\d[0-9]\)*\z/, message: 'Only positive number without spaces are allowed'}
 end
