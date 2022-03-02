14f6b2831f11d4746df20f982f2a79136bc3672f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-02 12:54:59 -0600

    faker added

--- Gemfile
+++ Gemfile
@@ -47,6 +47,9 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
 
+# FAKER
+gem 'faker'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '5521529085',
+            category: %w[chinese italian japanese french belgian].sample }
+
+[dishoom, pizza_east].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
